2d81d4e9264444490cfa8e22c1e7b4c3258bb938
danielkzu <daniel@cazzulino.com>
2012-11-19 03:02:00 -0300

    Build only the main project, and provide a full path relative to the script location.

--- Source/build.bat
+++ Source/build.bat
@@ -1,1 +1,1 @@
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Clide.sln /p:NuGet="true" /verbosity:normal /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild %~dp0\Core\Core.csproj /p:NuGet="true" /verbosity:normal /nr:false
