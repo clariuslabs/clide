affe3f625cf48863645746edd31543341fc18f6b
Daniel Cazzulino <daniel@cazzulino.com>
2013-10-29 15:24:31 -0300

    Created additional project for common cross-IDE implementations. Updated nuspec for each IDE. Refactored targets to reduce custom msbuild stuff per binding.

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild %~dp0\Src\Clide\NuGet.proj /verbosity:normal /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild %~dp0\Src\NuGet.proj /verbosity:normal /nr:false
