756220e949c630acba40b646147f1b0997f174a7
Daniel Cazzulino <daniel@cazzulino.com>
2014-01-09 14:28:11 -0300

    Updated build script

--- Src/GlobalAssemblyInfo.cs
+++ Src/GlobalAssemblyInfo.cs
@@ -20,8 +20,8 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("2.3")]
+[assembly: AssemblyFileVersion("2.3")]
 
 [assembly: AssemblyConfiguration(ThisAssembly.Configuration)]
 internal class ThisAssembly
