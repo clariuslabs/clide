0a9b3f6a695deb770b8e828307c252f2d2b441d6
Daniel Cazzulino <daniel@cazzulino.com>
2016-01-19 16:56:38 -0300

    Add missing file

---
+++ src/Clide/Adapters/SolutionToVsAdapter.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace Clide
+{
+	[Adapter]
+	class SolutionToVsAdapter :
+		IAdapter<SolutionExplorerNode, IVsHierarchyItem>,
+		IAdapter<SolutionExplorerNode, IVsHierarchy>,
+		IAdapter<SolutionNode, IVsSolution>,
+		IAdapter<ProjectNode, IVsProject>
+	{
+		IVsHierarchyItem IAdapter<SolutionExplorerNode, IVsHierarchyItem>.Adapt (SolutionExplorerNode from) => from?.HierarchyNode;
+
+		IVsHierarchy IAdapter<SolutionExplorerNode, IVsHierarchy>.Adapt (SolutionExplorerNode from) => from?.HierarchyNode.GetActualHierarchy ();
+
+		IVsSolution IAdapter<SolutionNode, IVsSolution>.Adapt (SolutionNode from) => from.HierarchyNode.GetServiceProvider ().GetService<SVsSolution, IVsSolution> ();
+
+		IVsProject IAdapter<ProjectNode, IVsProject>.Adapt (ProjectNode from) => from.HierarchyNode.GetActualHierarchy () as IVsProject;
+	}
+}
