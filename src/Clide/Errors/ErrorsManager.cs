5cebdfd52bf267182a15da248512070609059b8b
Adrian Alonso <adrianalonso@gmail.com>
2017-11-24 13:13:00 -0300

    Added IErrorsManager impl

---
+++ src/Clide/Errors/ErrorsManager.cs
@@ -0,0 +1,63 @@
+﻿namespace Clide
+{
+	using Microsoft.VisualStudio.Shell;
+	using System;
+	using System.ComponentModel.Composition;
+
+	[Export(typeof(IErrorsManager))]
+	[PartCreationPolicy(CreationPolicy.Shared)]
+	class ErrorsManager : IErrorsManager
+	{
+		readonly IServiceProvider serviceProvider;
+		readonly ErrorListProvider errorListProvider;
+
+		[ImportingConstructor]
+		public ErrorsManager([Import(typeof(SVsServiceProvider))] IServiceProvider serviceProvider)
+		{
+			this.serviceProvider = serviceProvider;
+			this.errorListProvider = new ErrorListProvider(this.serviceProvider);
+		}
+
+		public IErrorItem AddError(string text, Action<IErrorItem> handler)
+		{
+			return Add(text, handler, false);
+		}
+
+		public IErrorItem AddWarning(string text, Action<IErrorItem> handler)
+		{
+			return Add(text, handler, true);
+		}
+
+		public void ClearErrors()
+		{
+			this.errorListProvider.Tasks.Clear();
+		}
+
+		public void ShowErrors()
+		{
+			this.errorListProvider.Show();
+		}
+
+		private IErrorItem Add(string text, Action<IErrorItem> handler, bool isWarning)
+		{
+			var errorTask = new ErrorTask();
+
+			errorTask.Category = TaskCategory.Misc;
+			errorTask.ErrorCategory = isWarning ? TaskErrorCategory.Warning : TaskErrorCategory.Error;
+			errorTask.Text = text;
+			errorTask.Document = " ";
+
+			var errorItem = new ErrorItem(this.errorListProvider, errorTask);
+
+			errorTask.Navigate += (sender, e) =>
+			{
+				if (handler != null)
+					handler(errorItem);
+			};
+
+			this.errorListProvider.Tasks.Add(errorTask);
+
+			return errorItem;
+		}
+	}
+}
