860cfef1ee2ea8cbe68a64e1d8bb49212f8e8416
Adrian Alonso <adrianalonso@gmail.com>
2017-11-24 18:26:32 -0300

    Added Project.Configuration support

---
+++ src/Clide/Solution/ProjectConfiguration.cs
@@ -0,0 +1,50 @@
+﻿
+namespace Clide
+{
+	using EnvDTE;
+	using System;
+	using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class ProjectConfiguration : IProjectConfiguration
+    {
+        private Lazy<Project> project;
+
+        public ProjectConfiguration(Lazy<Project> project)
+        {
+            this.project = project;
+        }
+
+        public string ActiveConfigurationName
+        {
+            get { return this.ActiveConfiguration + "|" + this.ActivePlatform; }
+        }
+
+        public string ActiveConfiguration
+        {
+            get { return project.Value.ConfigurationManager.ActiveConfiguration.ConfigurationName; }
+        }
+
+        public string ActivePlatform
+        {
+            get { return project.Value.ConfigurationManager.ActiveConfiguration.PlatformName.Replace(" ", "").Trim(); }
+        }
+
+        public IEnumerable<string> Configurations
+        {
+            get { return ((IEnumerable)project.Value.ConfigurationManager.ConfigurationRowNames).OfType<string>(); }
+        }
+
+        public IEnumerable<string> Platforms
+        {
+            get
+            {
+                return ((IEnumerable)this.project.Value.ConfigurationManager.PlatformNames)
+                    .OfType<string>()
+                    // The configuration API does not use whitespaces.
+                    .Select(s => s.Replace(" ", "").Trim());
+            }
+        }
+    }
+}
