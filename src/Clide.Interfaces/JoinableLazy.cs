a7eb0711c2ea9f8cc8b7f592d5ca09a6c6a729c5
Mauro Agnoletti <mauro.agnoletti@gmail.com>
2018-07-30 16:10:08 -0300

    [asyncreview] - Added new JoinableLazy type to replace AsyncLazy usage
    JoinableLazy is an improved and more complete type alternative to AsyncLazy, which allows to define both async and sync value factories and also access the value in both ways (sync and async).
    Also, the usage of the UI thread can be explicitly defined by passing a constructor argument instead of doing it as part of the value factory

---
+++ src/Clide.Interfaces/JoinableLazy.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Threading;
+
+namespace Clide
+{
+    public class JoinableLazy<T>
+    {
+        static JoinableTaskContext sharedContext;
+        readonly JoinableTaskFactory taskFactory;
+        readonly AsyncLazy<T> asyncLazy;
+
+        public JoinableLazy(Func<Task<T>> valueFactory, bool executeOnMainThread = false)
+            : this(valueFactory, ThreadHelper.JoinableTaskFactory, executeOnMainThread)
+        {
+        }
+
+        public JoinableLazy(Func<Task<T>> valueFactory, JoinableTaskFactory taskFactory, bool executeOnMainThread = false)
+        {
+            this.taskFactory = taskFactory;
+            asyncLazy = new AsyncLazy<T>(async () => {
+                if (executeOnMainThread)
+                    await GetTaskFactory().SwitchToMainThreadAsync();
+
+                return await valueFactory();
+            }, GetTaskFactory());
+        }
+
+        public JoinableLazy(Func<T> valueFactory, bool executeOnMainThread = false)
+          : this(valueFactory, ThreadHelper.JoinableTaskFactory, executeOnMainThread)
+        {
+        }
+
+        public JoinableLazy(Func<T> valueFactory, JoinableTaskFactory taskFactory, bool executeOnMainThread = false)
+        {
+            this.taskFactory = taskFactory;
+            asyncLazy = new AsyncLazy<T>(async () => {
+                if (executeOnMainThread)
+                    await GetTaskFactory().SwitchToMainThreadAsync();
+
+                return valueFactory();
+            }, GetTaskFactory());
+        }
+
+
+        public static void SetContext (JoinableTaskContext context) => sharedContext = context;
+
+        public T GetValue() => GetTaskFactory().Run(async () => await asyncLazy.GetValueAsync());
+
+        public Task<T> GetValueAsync() => GetValueAsync(CancellationToken.None);
+
+        public Task<T> GetValueAsync(CancellationToken cancellationToken) => asyncLazy.GetValueAsync(cancellationToken);
+
+        JoinableTaskFactory GetTaskFactory() => sharedContext?.Factory ?? taskFactory;
+    }
+}
