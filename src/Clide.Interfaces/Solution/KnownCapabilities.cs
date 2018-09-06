399165c9ff24774d3926b0654bc7633ccff4e785
Daniel Cazzulino <daniel@cazzulino.com>
2018-09-06 15:57:54 -0300

    Added more known capabilities

--- src/Clide.Interfaces/Solution/KnownCapabilities.cs
+++ src/Clide.Interfaces/Solution/KnownCapabilities.cs
@@ -5,6 +5,46 @@ namespace Clide
     [Flags]
     public enum KnownCapabilities
     {
+        /// <summary>
+        /// The project is a shared assets project
+        /// </summary>
         SharedAssetsProject = 1,
+
+        /// <summary>
+        /// The project supports the .NET runtime.
+        /// </summary>
+        NET = 2,
+
+        /// <summary>
+        /// The project builds managed code.
+        /// </summary>
+        Managed = 4,
+
+        /// <summary>
+        /// The project supports the C# language.
+        /// </summary>
+        CSharp = 8,
+
+        /// <summary>
+        /// The project supports the VB language.
+        /// </summary>
+        VB = 16,
+
+        /// <summary>
+        /// The project supports Windows Universal XAML (i.e. it's a UWP app).
+        /// </summary>
+        WindowsXaml,
+
+        /// <summary>
+        /// The project supports Xamarin XAML (i.e. it's a Xamarin Android or iOS 
+        /// app or library).
+        /// </summary>
+        XamarinXaml,
+
+        /// <summary>
+        /// The project has the Xamarin.Forms nuget package installed and restored 
+        /// (important since the capability comes from an imported targets file).
+        /// </summary>
+        XamarinForms,
     }
 }
