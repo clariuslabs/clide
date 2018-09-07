efa8f0ebb5ce1ae8ed9ba143bb443c5a0a5c9d39
Adrian Alonso <adrianalonso@gmail.com>
2018-09-07 16:32:40 -0300

    Added IVsHierarchy -> IProjectNode adapter

---
+++ src/Clide.Interfaces/Adapters/VsAdapterFacade.cs
@@ -0,0 +1,12 @@
+﻿using Clide;
+using Microsoft.VisualStudio.Shell.Interop;
+
+public static partial class AdapterFacade
+{
+    /// <summary>
+    /// Adapts a <see cref="IVsHierarchy"/> to an <see cref="IProjectNode"/>.
+    /// </summary>
+    /// <returns>The <see cref="IProjectNode"/> or <see langword="null"/> if conversion is not possible.</returns>
+    public static IProjectNode AsProjectNode(this IVsHierarchy project) =>
+        project.GetServiceLocator().GetExport<IAdapterService>().Adapt(project).As<IProjectNode>();
+}
