5571831b1e7bf76122a5a53ac4a25eca16d19b3d
Juan Marcelo Tondato <jmt@xamarin.com>
2018-10-02 09:07:47 -0300

    Adds SolutionNode to ENVDTE.Solution adapter

---
+++ src/Clide/Adapters/SolutionNodeToDte.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell;
+
+namespace Clide.Adapters
+{
+    [Adapter]
+    class SolutionNodeToDte : IAdapter<SolutionNode, EnvDTE.Solution>
+    {
+        public EnvDTE.Solution Adapt(SolutionNode from) {
+            return ThreadHelper.JoinableTaskFactory.Run(async () =>
+            {
+                await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+                var dte = ServiceLocator.Global.GetService<EnvDTE.DTE>();
+
+                return dte.Solution;
+            });
+        }
+    }
+}
