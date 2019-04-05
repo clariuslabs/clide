0cda9fa49e7ea84a603f009b30beba6f8e8a8695
Adrian Alonso <adrianalonso@gmail.com>
2019-04-05 17:52:25 -0300

    Using FlavoredProjectBase to check if we should use the IVsHierarchy (#129)
    We must not call Get IVsHierarchyItemManager.GetHierarchyItem if the
    corresponding IVsHierarchy is the flavored instance.

--- src/Clide/Adapters/VsToSolutionAdapter.cs
+++ src/Clide/Adapters/VsToSolutionAdapter.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Flavor;
 using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.VisualStudio.Threading;
 using System;
@@ -28,7 +29,7 @@ namespace Clide
         }
 
         public IProjectNode Adapt(IVsHierarchy from) =>
-            from is FlavoredProject && from.TryGetInnerHierarchy(out var innerHierarchy) ?
+            from is FlavoredProjectBase && from.TryGetInnerHierarchy(out var innerHierarchy) ?
                 Adapt(new FlavoredProject(from, innerHierarchy)) :
                 nodeFactory
                     .Value
