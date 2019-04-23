548c08c886cb3519ff48caba99022e0eed889c59
Daniel Cazzulino <daniel@cazzulino.com>
2019-04-23 02:05:03 -0300

    Limit minimum VS version to current in Debug builds
    When building in debug mode, we use the current VS version to limit the target VS and max/min versions, so that tests only run against the current VS version. We also disable recycling since in debug builds, we'll most likely want to see tests fail once and fix them.

--- src/Clide.IntegrationTests/Properties/AssemblyInfo.cs
+++ src/Clide.IntegrationTests/Properties/AssemblyInfo.cs
@@ -6,3 +6,15 @@ using Xunit;
 #if DEBUG
 [assembly: VsixRunner(ProcessStartRetries = 1, RemoteConnectionRetries = 1, DebuggerAttachRetries = 5, StartupTimeout = 300)]
 #endif
+
+#if DEBUG
+// Limit run to current VS version
+[assembly: Vsix(ThisAssembly.Metadata.VisualStudioVersion, 
+    MinimumVisualStudioVersion = ThisAssembly.Metadata.VisualStudioVersion, 
+    MaximumVisualStudioVersion = ThisAssembly.Metadata.VisualStudioVersion,
+    TimeoutSeconds = 120, 
+    // No need to recycle on local dev runs
+    RecycleOnFailure = false)]
+#else
+[assembly: Vsix("16.0", MinimumVisualStudioVersion = "16.0", TimeoutSeconds = 120, RecycleOnFailure = true)]
+#endif
