2c55abbcf9cb2c262018f087dfe2feb022539cde
Adrian Alonso <adrianalonso@gmail.com>
2019-09-06 12:27:04 -0300

    Pushing DialogCreated event when a dialog is created
    This will allow client to be notified when dialog instances
    are created and execute and do proper stuff such as logging or
    telemetry.

---
+++ src/Clide.Interfaces/DialogCreated.cs
@@ -0,0 +1,12 @@
+﻿namespace Clide
+{
+    public class DialogCreated
+    {
+        public DialogCreated(IDialogWindow dialog)
+        {
+            Dialog = dialog;
+        }
+
+        public IDialogWindow Dialog { get; }
+    }
+}
